6f85931fab029ee29275f9374446795df60f2fdc
DESKTOP-3I4UA3R\AREGBESOLA .BOLU <justicearegbesola@gmail.com>
2025-01-08 04:18:22 -0800

    resolved conflict

--- js/slider.js
+++ js/slider.js
@@ -12,7 +12,32 @@ $(document).ready(function () {
     prevArrow: '<div class="chevron chevron-left">&#10094;</div>',
   });
 
-  // second Slider
+  // Second Slider
+  $(".new-slider").slick({
+    Infinite: false,
+    dots: true, // Enable navigation dots
+    arrows: false, // Hide navigation arrows
+    slidesToShow: 4, // Show 4 slides at once
+    slidesToScroll: 1, // Move 1 slide at a time
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 768, // Tablet view
+        settings: {
+          slidesToShow: 2, // Show 2 slides at once
+        },
+      },
+      {
+        breakpoint: 480, // Mobile view
+        settings: {
+          slidesToShow: 1, // Show 1 slide at a time
+        },
+      },
+    ],
+  });
+
+  // Third Slider
   $(".packages-slider").slick({
     dots: true,
     arrows: true,
